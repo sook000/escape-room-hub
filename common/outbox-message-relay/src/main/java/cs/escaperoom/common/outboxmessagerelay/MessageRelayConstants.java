6ade40fb4dbfffb6e9a1a2101f9524ebb6622bbf
안치숙 <songweddin@naver.com>
2025-09-26 09:45:30 +0900

    Feat: 인기글을 위한 Transactional Outbox 모듈 구현
    advanced-board의 인기글을 위한 Transactional Outbox 모듈 구현까지 반영

---
+++ common/outbox-message-relay/src/main/java/cs/escaperoom/common/outboxmessagerelay/MessageRelayConstants.java
@@ -0,0 +1,9 @@
+package cs.escaperoom.common.outboxmessagerelay;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class MessageRelayConstants {
+    public static final int SHARD_COUNT = 4; //임의의 값
+
+}
---
+++ common/outbox-message-relay/src/main/java/cs/escaperoom/common/outboxmessagerelay/OutboxEventPublisher.java
@@ -0,0 +1,29 @@
+package cs.escaperoom.common.outboxmessagerelay;
+
+import cs.escaperoomhub.common.event.Event;
+import cs.escaperoomhub.common.event.EventPayload;
+import cs.escaperoomhub.common.event.EventType;
+import cs.escaperoomhub.common.snowflake.Snowflake;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class OutboxEventPublisher {
+    private final Snowflake outboxIdSnowflake = new Snowflake();
+    private final Snowflake eventIdSnowflake = new Snowflake();
+    private final ApplicationEventPublisher applicationEventPublisher;
+
+    public void publish(EventType type, EventPayload payload, Long shardKey) {
+        Outbox outbox = Outbox.create(
+                outboxIdSnowflake.nextId(),
+                type,
+                Event.of(
+                        eventIdSnowflake.nextId(), type, payload
+                ).toJson(),
+                shardKey % MessageRelayConstants.SHARD_COUNT
+        );
+        applicationEventPublisher.publishEvent(OutboxEvent.of(outbox));
+    }
+}
